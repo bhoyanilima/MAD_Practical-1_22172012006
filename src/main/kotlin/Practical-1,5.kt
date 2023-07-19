93f20bfd8e1c371c1ea723b8a6e5d173c657674d
Student <student@gnu.ac.in>
2023-07-19 09:00:09 +0530

    hello

---
+++ src/main/kotlin/Practical-1,5.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    var a=10;
+    when (a)
+    {
+        1 -> println("january")
+        2 -> println("February")
+        3 -> println("march")
+        4 -> println("April")
+        5 -> println("may")
+        6 -> println("m")
+
+
+    }
+}
